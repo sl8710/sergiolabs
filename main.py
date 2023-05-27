9ceef551222cdd386c399a6782fa16f5e49482a8
sl8710 <132164550+sl8710@users.noreply.github.com>
2023-05-27 00:03:11 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import time
+from Stopwatch import *
+from Button import *
+from Counters import *
+from Displays import *
+
+time.sleep(0.1) # Wait for USB to become ready
+
+s = StopWatch()
+s.run()
